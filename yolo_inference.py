e75102bfa3232d1dc0364da8d3b2109b68102018
ehz0ah <haozhe4547@gmail.com>
2024-10-11 22:06:32 +0800

    Replace yolo v8 with trained model

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov8x')   # Largest model (68.2M params)
+model = YOLO('models/best.pt')
 
 results = model.predict('input_videos/08fd33_4.mp4',save=True)
 print(results[0])
