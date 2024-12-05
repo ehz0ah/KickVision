6749393ff10a885dfcb17ac08b1e9a90530d307e
ehz0ah <haozhe4547@gmail.com>
2024-12-05 23:26:02 +0800

    Add test code for cropping images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import Tracker
+import cv2
 
 def main():
     # Read the video
@@ -12,6 +13,20 @@ def main():
                                       read_from_stubs=True,
                                       stub_path='stubs/track_stubs.pkl')
     
+    '''
+    # Save cropped image of a player
+    for track_id, player in tracks['players'][0].items():
+        bbox = player['bbox']
+        frame = video_frames[0]
+
+        # Crop bbox from frame
+        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
+
+        # Save the cropped image
+        cv2.imwrite(f'cropped_images/cropped_image.jpg', cropped_image)
+        break
+    '''
+        
     # Draw output
     ## Draw object tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
